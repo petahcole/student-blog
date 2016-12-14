62d7691d37463afdb7662cdfc14089bb7c99a23b
petahcole <cbatson89@gmail.com>
2016-12-14 11:34:06 -0700

    update redirect

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res,)  =>  {
+  res.redirect("/blogs");
+})
 app.use('/', index);
 
 app.use('/users', users);
