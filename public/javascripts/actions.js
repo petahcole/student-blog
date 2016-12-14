a18652ec8399aba676ecd3cb6d938c220dcbe2f1
petahcole <cbatson89@gmail.com>
2016-12-14 11:07:40 -0700

    DELETE PUT and Validation

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -1,1 +1,26 @@
 console.log("working!")
+
+
+$(() => {
+  $('#delete').click(function () {
+    let id = $(this).attr('data-id');
+    $.ajax({
+      method: 'delete',
+      url: `/blogs/${id}`,
+      success: function () {
+        location = "/blogs";
+      },
+      fail: function (err) {
+        console.log(err);
+      }
+    });
+  })
+})
+
+function validateForm() {
+    let $form = $("form").val()
+    if ($form == "" || typeof $form !== "string") {
+        alert("Invalid input");
+        return false;
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -30,8 +30,42 @@ router.post("/blogs", (req, res, next) =>  {
 })
 
 router.delete("/blogs/:id", (req, res, next)  =>  {
-  knex("blogs").where("id", req,params.id).delete().then((id) =>{
+  knex("blogs").where("id", req.params.id).delete().then((id) =>  {
     res.send("Bye bye now.")
   })
 })
+
+router.get("/edit/:id", (req, res, next)  =>  {
+knex("blogs").where("id", req.params.id).first().then((blog)  =>  {
+  res.render("edit", {blog: blog})
+  })
+})
+
+router.put("/blogs/:id", (req, res, next) =>  {
+if (isValidPost(req.body))  {
+    knex("blogs").where("id", req.params.id).update({
+      "firstName": req.body.firstName,
+      "lastName": req.body.lastName,
+      "cohortID": req.body.cohortID,
+      "post": req.body.post
+      }).then((blog) =>  {
+      res.redirect("/blogs")
+    })
+  } else {
+    res.render("error", {message: "Invalid Input"})
+  }
+})
+
+function isValidPost(post)  {
+  if (!post) {
+    return false
+  } 
+  if (!post.firstName || !post.lastName || !post.cohortID || !post.post)  {
+    return false
+  }
+  if (typeof post.firstName !== "string" || typeof post.lastName !== "string" || typeof post.cohortID !== "string" || typeof post.post !== "string")  {
+    return false
+  }
+  return true
+} 
 module.exports = router;
